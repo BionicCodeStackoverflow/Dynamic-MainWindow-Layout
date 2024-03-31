2e046da97bbbc013a8b0f03cce60be114cb76795
BionicCode <BionicCode@outlook.com>
2024-03-31 15:06:21 +0200

    Remove debug folder from source control

--- Main/obj/Debug/net8.0-windows/Main.AssemblyInfo.cs
+++ Main/obj/Debug/net8.0-windows/Main.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f9bf7a6fd8d52809adf26ef8e60401de503eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
